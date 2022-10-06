dba86a97684d0a69f44d464568ea574807f9d7de
Alex Beltyukov <xonika9@gmail.com>
2022-10-06 13:47:23 +0300

    add regex, error codes and messages

---
+++ utils/ErrorMessages.js
@@ -0,0 +1,21 @@
+const serverErrorMessage = 'Server error';
+const badRequestMessage = 'Enter valid data';
+const authorizationErrorMessage = 'Authorization error';
+const wrongEmailOrPasswordMessage = 'Wrong email or password';
+const forbiddenCardDeleteMessage = 'You can only delete your own card';
+const userNotFoundMessage = 'User not found';
+const cardNotFoundMessage = 'Card not found';
+const pageNotFoundMessage = 'Page not found';
+const emailAlreadyTakenMessage = 'The email has already been taken';
+
+module.exports = {
+  serverErrorMessage,
+  badRequestMessage,
+  authorizationErrorMessage,
+  wrongEmailOrPasswordMessage,
+  forbiddenCardDeleteMessage,
+  userNotFoundMessage,
+  cardNotFoundMessage,
+  pageNotFoundMessage,
+  emailAlreadyTakenMessage,
+};
