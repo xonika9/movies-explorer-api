a71cd87c5dfd35e8e00bf5243cffdc6482849bd4
Alex Beltyukov <xonika9@gmail.com>
2022-10-07 13:42:25 +0300

    add logger, errorHandler, rate limiter, helmet

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-// const cookieParser = require('cookie-parser');
-// const { errors } = require('celebrate');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 // const cors = require('cors');
-// const router = require('./routes/index');
-// const errorHandler = require('./middlewares/error');
-// const { requestLogger, errorLogger } = require('./middlewares/logger');
+const helmet = require('helmet');
+const router = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -14,21 +15,21 @@ const app = express();
 
 // app.use(
 //   cors({
-//     origin: ['http://localhost:3000', 'http://x9-mesto.nomoredomains.sbs', 'https://x9-mesto.nomoredomains.sbs'],
+//     origin: ['http://localhost:3000'],
 //     credentials: true,
 //   }),
 // );
 
-// app.use(requestLogger);
-
 app.use(express.json());
-// app.use(cookieParser());
-// app.use('/', router);
+app.use(helmet());
 
-// app.use(errorLogger);
+app.use(cookieParser());
+app.use('/', router);
 
-// app.use(errors());
-// app.use(errorHandler);
+app.use(errors());
+app.use(requestLogger);
+app.use(errorLogger);
+app.use(errorHandler);
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/movexdb', {
